53595930fdde00f64d8bb1eac3e7f70a74dd1e43
benedict button <benedictdaly@hotmail.com>
2023-01-05 08:49:50 -0500

    Changes to use hashRouter

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import {
   Route,
   Link,
   Outlet,
+  NavLink,
 } from "react-router-dom";
+import { Card } from "@grafana/ui";
 import Grafana from "./Grafana";
 import ReactTable from "./ReactTable";
 
@@ -13,6 +15,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <NavLink to="/grafana">
+          <Card>
+            <Card.Heading>Grafana Table</Card.Heading>
+          </Card>
+        </NavLink>
+        <NavLink to="/react-table">
+          <Card>
+            <Card.Heading>React-Table</Card.Heading>
+          </Card>
+        </NavLink>
         <Outlet />
       </header>
     </div>
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
 import Grafana from "./Grafana";
 import ReactTable from "./ReactTable";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
