1b4fdb274c2e7188470f9ea24b344aa19a216839
benedict button <benedictdaly@hotmail.com>
2023-01-06 16:51:57 -0500

    Removes unnecessary react-table code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
 import React, { useState } from "react";
-import { Outlet, NavLink } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { Card } from "@grafana/ui";
+import Grafana from "./Grafana";
+import ReactTable from "./ReactTable";
 
 function App() {
   const [selected, setSelected] = useState("grafana");
@@ -16,42 +18,39 @@ function App() {
           }}
         >
           <div className="nav-cards">
-            <NavLink to="/grafana" style={{ textDecoration: "none" }}>
-              <Card
-                isSelected={selected === "grafana"}
-                onClick={() => setSelected("grafana")}
-              >
-                <Card.Heading>
-                  <h3>Grafana</h3>
-                </Card.Heading>
-                <Card.Description>
-                  New to Grafana and struggled finding adequate developer
-                  documentation. The storybook UI seems like very much a work in
-                  progress.
-                </Card.Description>
-              </Card>
-            </NavLink>
+            <Card
+              isSelected={selected === "grafana"}
+              onClick={() => setSelected("grafana")}
+            >
+              <Card.Heading>
+                <h3>Grafana</h3>
+              </Card.Heading>
+              <Card.Description>
+                New to Grafana and struggled finding adequate developer
+                documentation. The storybook UI seems like very much a work in
+                progress.
+              </Card.Description>
+            </Card>
           </div>
           <div className="nav-cards">
-            <NavLink to="/react-table" style={{ textDecoration: "none" }}>
-              <Card
-                isSelected={selected === "react-table"}
-                onClick={() => setSelected("react-table")}
-              >
-                <Card.Heading>
-                  <h3>React-Table</h3>
-                </Card.Heading>
-                <Card.Description>
-                  Despite Grafana abstracting react-table in its UI table, I
-                  found using react-table directly easier as it gave me more
-                  options and finer control.
-                </Card.Description>
-              </Card>
-            </NavLink>
+            <Card
+              isSelected={selected === "react-table"}
+              onClick={() => setSelected("react-table")}
+            >
+              <Card.Heading>
+                <h3>React-Table</h3>
+              </Card.Heading>
+              <Card.Description>
+                Despite Grafana abstracting react-table in its UI table, I found
+                using react-table directly easier as it gave me more options and
+                finer control.
+              </Card.Description>
+            </Card>
           </div>
         </div>
       </header>
-      <Outlet />
+      {selected === "grafana" && <Grafana />}
+      {selected === "react-table" && <ReactTable />}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,31 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { createHashRouter, RouterProvider } from "react-router-dom";
-import Grafana from "./Grafana";
-import ReactTable from "./ReactTable";
-
-const router = createHashRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "grafana",
-        element: <Grafana />,
-      },
-      {
-        path: "react-table",
-        element: <ReactTable />,
-      },
-    ],
-  },
-]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
 
